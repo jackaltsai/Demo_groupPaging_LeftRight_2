3e215dd072d932515c14663e47880dcf5b220e76
jackaltsai <hata.s520@gmail.com>
2021-09-21 17:23:42 +0800

    group 裡有兩個 item

--- Demo_groupPaging_LeftRight_2/ViewController.swift
+++ Demo_groupPaging_LeftRight_2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    // 範例 2: group 裡有兩個 item
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = generateLayout()
+        collectionView.isScrollEnabled = false
     }
 
-
+    func generateLayout() -> UICollectionViewLayout {
+        let space: Double = 5
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: space, bottom: 0, trailing: space)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .fractionalHeight(1))
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = .fixed(space * 2)
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CollectionViewCell.self)", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "Image\(indexPath.item + 1)")
+        cell.label.text = "Image\(indexPath.item + 1)"
+        return cell
+    }
+    
+    
+}
